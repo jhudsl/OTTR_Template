206e5dcb879e863cd11ebf7820d60c40da11787b
Candace Savonen <cansav09@gmail.com>
2021-05-04 12:58:24 -0400

    Push Google refresh toekn (#97)

--- scripts/util/google-slides.R
+++ scripts/util/google-slides.R
@@ -16,8 +16,9 @@ authorize_from_secret <- function(access_token, refresh_token) {
     scope = "https://www.googleapis.com/auth/presentations https://www.googleapis.com/auth/drive.readonly", 
     token_type = "Bearer")
   
-  app <- httr::oauth_app(appname = "googleslides", key = client_id, 
-                   secret = client_secret)
+  app <- httr::oauth_app(appname = "googleslides", 
+                         key = client_id, 
+                         secret = client_secret)
   endpoint <- httr::oauth_endpoints("google")
   
   token <- httr::oauth2.0_token(endpoint = endpoint, app = app, 
