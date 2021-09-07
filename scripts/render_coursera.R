8e86c91c3361c218e5ae1e2e614b72282362a9c0
Candace Savonen <cansav09@gmail.com>
2021-09-07 13:10:56 -0400

    Ah. clean_envir = TRUE strikes again

--- scripts/render_coursera.R
+++ scripts/render_coursera.R
@@ -7,18 +7,21 @@
 # Usage (from Terminal):
 # Rscript -e "render_all_coursera.R"
 
+# Clean out environment before we start
+rm(list = ls())
+
 # Assumes script is located in the scripts folder
 root_dir <- rprojroot::find_root("_bookdown.yml")
 
 # Create output folder
-output_directory <- file.path("docs", "coursera")
+output_dir <- file.path("docs", "coursera")
 
-if (!dir.exists(output_directory)) {
-  dir.create(output_directory, recursive = TRUE, showWarnings = FALSE)
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 }
 
 # Clean out old files if they exist
-old_files <- list.files(output_directory, pattern = c("html$", "md$"), full.names = TRUE)
+old_files <- list.files(output_dir, pattern = c("html$", "md$"), full.names = TRUE)
 if (length(old_files) > 0) {
   file.remove(old_files)
 }
@@ -27,37 +30,43 @@ if (length(old_files) > 0) {
 needed_directories <- c("assets", "code_output", "resources")
 
 lapply(needed_directories, function(needed_dir) {
-  if (!dir.exists(file.path(output_directory, needed_dir))) {
-    fs::dir_copy(needed_dir, file.path(output_directory, needed_dir), overwrite = TRUE)
+  if (!dir.exists(file.path(output_dir, needed_dir))) {
+    fs::dir_copy(needed_dir, file.path(output_dir, needed_dir), overwrite = TRUE)
   }
 })
 
 # Slightly different path for the libs folder
 libs_path <- file.path("docs", "libs")
-if (!dir.exists(file.path(output_directory, "libs"))) {
-  fs::dir_copy(libs_path, file.path(output_directory, "libs"), overwrite = TRUE)
+if (!dir.exists(file.path(output_dir, "libs"))) {
+  fs::dir_copy(libs_path, file.path(output_dir, "libs"), overwrite = TRUE)
 }
 
 # Retrieve list of Rmd files from the _bookdown.yml
 output_yaml <- yaml::yaml.load_file(file.path(root_dir, "_output.yml"))
 
 # Change CSS file to coursera special one
-output_yaml$`bookdown::gitbook`$css <- gsub("\\.css", "_coursera.css", output_yaml$`bookdown::gitbook`$css)
+coursera_css <- gsub("\\.css", "_coursera.css", output_yaml$`bookdown::gitbook`$css)
+output_yaml$`bookdown::gitbook`$css <- coursera_css
 
 # Write this new coursera yml
-yaml::write_yaml(output_yaml, file.path(output_directory, "_output_coursera.yml"))
+yaml::write_yaml(output_yaml, file.path(output_dir, "_output_coursera.yml"))
 
 
-message("render bookdown")
+message("Render bookdown without TOC for Coursera")
+
 # Do the render
 bookdown::render_book(
   input = "index.Rmd",
-  output_yaml = file.path(output_directory, "_output_coursera.yaml"),
-  output_dir = file.path("docs", "coursera"))
+  output_yaml = file.path(output_dir, "_output_coursera.yaml"),
+  output_dir = file.path(output_dir, "coursera"),
+  clean_envir = FALSE)
+
+# Get specified style name
+style_css <- file.path(coursera_css)
 
 # Read in TOC closing CSS lines
-toc_close_css <- readLines(output_yaml$`bookdown::gitbook`$css)
-full_css <- readLines(file.path(output_directory, "assets", "style.css"))
+toc_close_css <- readLines(style_css)
+full_css <- readLines(file.path(output_dir, "assets", "style.css"))
 
 # Write to "style.css"
-writeLines(append(full_css, toc_close_css), file.path(output_directory, "assets", "style.css"))
+writeLines(append(full_css, toc_close_css), file.path(output_dir, "assets", "style.css"))
