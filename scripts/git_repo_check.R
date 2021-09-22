bf8fa502a0f7e6644721827bd2cff4b599426b06
Candace Savonen <cansav09@gmail.com>
2021-09-22 11:43:18 -0400

    Try other bash thing

--- scripts/git_repo_check.R
+++ scripts/git_repo_check.R
@@ -3,7 +3,7 @@
 # Written by Candace Savonen Sept 2021
 
 repo <- commandArgs(trailingOnly = TRUE)
-repo <- "jhudsl/DaSL_Course_Template_Bookdown"
+
 check_git_repo <- function(repo, silent = TRUE, return_repo = FALSE) {
   # Given a repository name, check with git ls-remote whether the repository 
   # exists and return a TRUE/FALSE
