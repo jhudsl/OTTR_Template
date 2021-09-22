4b32d3aeb516e6765cf4907070e9327d3c5b71d5
Candace Savonen <cansav09@gmail.com>
2021-09-22 12:41:14 -0400

    Add git repo checker step in transfer-rendered-files.yml (#231)
    * Add git_check_repo script
    
    * Add git_repo_check script and function
    
    * Fix spacing
    
    * Git rid of remnant step
    
    * Add quotes
    
    * Fix R script call
    
    * Fix it again Lol
    
    * Maybe the echo is the problem
    
    * Add string manipulation to R script instead
    
    * Try other bash thing
    
    * Test which step is the problem
    
    * Fix one ref
    
    * See if brackets are the issue
    
    * Just grasping for straws here
    
    * I'm such a dummy. The script doesn't exist there
    
    * Install svn
    
    * Make sure we can find the file.
    
    * Test didn't work
    
    * Add GITHUB REF to PR message
    
    * Fix repo designation

---
+++ scripts/git_repo_check.R
@@ -0,0 +1,60 @@
+#!/usr/bin/env Rscript
+
+# Written by Candace Savonen Sept 2021
+
+repo <- commandArgs(trailingOnly = TRUE)
+
+if (!is.character(repo)) {
+  repo <- as.character(repo)
+}
+
+check_git_repo <- function(repo, silent = TRUE, return_repo = FALSE) {
+  # Given a repository name, check with git ls-remote whether the repository 
+  # exists and return a TRUE/FALSE
+  
+  # Inputs: 
+  # repo: the name of the repository, e.g. jhudsl/DaSL_Course_Template_Bookdown
+  # silent: TRUE/FALSE of whether the warning from the git ls-remote command 
+  #         should be echoed back if it does fail.
+  # return_repo: TRUE/FALSE of whether or not the output from git ls-remote 
+  #              should be saved to a file (if the repo exists)
+  
+  # Returns: 
+  # A TRUE/FALSE whether or not the repository exists. 
+  # Optionally the output from git ls-remote if return_repo = TRUE. 
+  
+  message(paste("Checking for remote git repository:", repo))
+
+  # If silent = TRUE don't print out the warning message from the 'try'
+  report <- ifelse(silent, suppressWarnings, message)
+
+  # Try to git ls-remote the repo given
+  test_repo <- report(
+    try(system(paste0("git ls-remote https://github.com/", repo),
+              intern = TRUE, ignore.stderr = TRUE))
+    )
+
+  # If 128 is returned as a status attribute it means it failed
+  exists <- ifelse(is.null(attr(test_repo, "status")), TRUE, FALSE)
+                 
+  if (return_repo && exists) {
+    # Make file name
+    output_file <- paste0("git_ls_remote_", gsub("/", "_", repo))
+    
+    # Tell the user the file was saved
+    message(paste("Saving output from git ls-remote to file:", output_file))
+    
+    # Write to file
+    writeLines(exists, file.path(output_file))
+  }
+
+  return(exists)
+}
+
+
+# Change repo name to its Leanpub equivalent: 
+repo <- gsub("Bookdown", "", repo)
+repo <- paste0(repo, "Leanpub")
+
+# Print out the result
+write(check_git_repo(repo), stdout())
