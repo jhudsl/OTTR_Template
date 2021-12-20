e8fa66d59dfb58d8ef1d7681c8e05ed8d8d9301f
Candace Savonen <cansav09@gmail.com>
2021-12-19 21:39:26 -0500

    Let's see if this works!

---
+++ scripts/get_spell_errors.R
@@ -0,0 +1,67 @@
+#!/usr/bin/env Rscript
+
+# Written by Candace Savonen Dec 2021
+
+# Ran by GitHub actions, given a repository name and a GitHub PAT, get the most recent spell check results.
+
+if (!("optparse" %in% installed.packages())){
+  install.packages("optparse")
+}
+
+library(magrittr)
+library(optparse)
+
+option_list <- list(
+  optparse::make_option(
+    c("--repo"),
+    type = "character",
+    default = NULL,
+    help = "GitHub repository name, e.g. jhudsl/OTTR_Template",
+  ),
+  optparse::make_option(
+    c("--git_pat"),
+    type = "character",
+    default = NULL,
+    help = "GitHub personal access token",
+  )
+)
+
+# Read the arguments passed
+opt_parser <- optparse::OptionParser(option_list = option_list)
+opt <- optparse::parse_args(opt_parser)
+
+repo_name <- opt$repo
+git_pat <- opt$git_pat
+
+if (!is.character(repo)) {
+  repo <- as.character(repo)
+}
+
+# Declare URL
+url <- paste0("https://api.github.com/repos/", repo_name, "/actions/artifacts")
+  
+# Github api get
+response <- httr::GET(
+  url,
+  httr::add_headers(Authorization = paste0("token ", git_pat)),
+  httr::accept_json()
+)
+  
+if (httr::http_error(response)) {
+  warning(paste0("url: ", url, " failed"))
+}
+  
+# Get content as JSON
+artifacts <- httr::content(response, as = "parsed")$artifacts
+
+# Make it a data frame
+artifacts_df <- do.call(rbind.data.frame, artifacts)
+
+# We only care about the spell check results and the most recent one
+the_url <- artifacts_df %>% 
+  dplyr::filter(name == "spell-check-results") %>% 
+  dplyr::top_n(1, created_at) %>%
+  dplyr::pull(archive_download_url)
+  
+# Print out the result
+write(the_url, stdout())
