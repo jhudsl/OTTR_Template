9d154da75f19205ca3465bd5b31caa4be6cc10e4
Candace Savonen <cansav09@gmail.com>
2022-02-07 20:48:30 -0500

    Add some config mechs

--- scripts/employ-gha-switches.R
+++ scripts/employ-gha-switches.R
@@ -1,8 +1,12 @@
 #!/usr/bin/env Rscript
 
+library(magrittr) 
 
 # Find .git root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".github"))
+
+# Load in what the on trigger criteria is
+on_triggers <- yaml::yaml.load_file(file.path(root_dir, ".github", "triggers.yml"))
 
 # Get github actions directory
 github_actions_dir <- file.path(root_dir, ".github", "workflows")
@@ -11,20 +15,23 @@ github_actions_dir <- file.path(root_dir, ".github", "workflows")
 github_actions_files <- list.files(github_actions_dir, pattern = "\\.yml$", 
                                    full.names = TRUE)
 
+config_yaml <- yaml::read_yaml(file.path(root_dir, "config_automation.yml")) %>% 
+  data.frame() %>% 
+  t() %>% 
+  data.frame() %>%
+  tibble::rownames_to_column("gha_file")
+
+colnames(config_yaml)[2] <- "on_or_off"
+
+gha_file <- github_actions_files[1]
 
 # Read in all files
-all_gha <- lapply(github_actions_files, yaml::yaml.load_file)
-names(all_gha) <- basename(github_actions_files)
+all_gha <- lapply(github_actions_files, function(gha_file) {
+  yaml_contents <- yaml::yaml.load_file(gha_file)
 
-# Extract trigger criteria
-gha_triggers <- lapply(1:length(all_gha), function(gha) purrr::pluck(all_gha, gha, "TRUE"))
-names(gha_triggers) <- basename(github_actions_files)
-
-
-yaml::write_yaml(gha_triggers, file.path(root_dir, ".github", "gha-triggers.yml"))
-
-# Switch out repo 
-yaml_contents$group$repos <- opt$repo
-
-yaml::write_yaml(yaml_contents, sync_file_path)
-
+  yaml_contents$
+  if ()
+  yaml_contents$`TRUE` <- on_triggers[[basename(gha_file)]]
+  
+  yaml::write_yaml(yaml_contents, sync_file_path)
+})
