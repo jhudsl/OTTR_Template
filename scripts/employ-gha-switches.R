964e16d4543f8d80ac4e8159f09886750f72b738
Candace Savonen <cansav09@gmail.com>
2022-02-08 08:18:22 -0500

    One strategy for yamls

--- scripts/employ-gha-switches.R
+++ scripts/employ-gha-switches.R
@@ -5,8 +5,12 @@ library(magrittr)
 # Find .git root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".github"))
 
-# Load in what the on trigger criteria is
+### Load in what the on trigger criteria is
 on_triggers <- yaml::yaml.load_file(file.path(root_dir, ".github", "triggers.yml"))
+
+###
+
+off_triggers <- 
 
 # Get github actions directory
 github_actions_dir <- file.path(root_dir, ".github", "workflows")
@@ -19,7 +23,8 @@ config_yaml <- yaml::read_yaml(file.path(root_dir, "config_automation.yml")) %>%
   data.frame() %>% 
   t() %>% 
   data.frame() %>%
-  tibble::rownames_to_column("gha_file")
+  tibble::rownames_to_column("gha_files") %>% 
+  dplyr::mutate(gha_files = paste0(gsub("\\.", "-", gha_files), ".yml"))
 
 colnames(config_yaml)[2] <- "on_or_off"
 
@@ -28,10 +33,17 @@ gha_file <- github_actions_files[1]
 # Read in all files
 all_gha <- lapply(github_actions_files, function(gha_file) {
   yaml_contents <- yaml::yaml.load_file(gha_file)
-
-  yaml_contents$
-  if ()
-  yaml_contents$`TRUE` <- on_triggers[[basename(gha_file)]]
   
-  yaml::write_yaml(yaml_contents, sync_file_path)
+  status <- config_yaml %>% 
+    dplyr::filter(basename(gha_file) == gha_files) %>% 
+    dplyr::pull("on_or_off")
+  
+  if (status) {
+    yaml_contents$`TRUE` <- on_triggers[[basename(gha_file)]]
+    names(yaml_contents)[2] <- yaml::as.yaml("on")
+  } else {
+    yaml_contents$`TRUE` <- "workflow_dispatch:"
+    names(yaml_contents)[2] <- yaml::as.yaml("on")
+  }
+  yaml::write_yaml(yaml_contents, "testing.yml")
 })
