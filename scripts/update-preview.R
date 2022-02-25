ff53b40688c674c30e7f2989eb1a083ef0a23f42
Candace Savonen <cansav09@gmail.com>
2022-02-25 10:33:26 -0500

    Update preview script

---
+++ scripts/update-preview.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+# Written by Candace Savonen Sept 2021
+
+if (!("optparse" %in% installed.packages())){
+  install.packages("optparse")
+}
+
+library(optparse)
+
+option_list <- list(
+  optparse::make_option(
+    c("--repo"),
+    type = "character",
+    default = NULL,
+    help = "GitHub repository name, e.g. jhudsl/OTTR_Template",
+  ),
+  optparse::make_option(
+    c("--git_pat"),
+    type = "character",
+    default = NULL,
+    help = "GitHub personal access token",
+  )
+)
+
+# Read the arguments passed
+opt_parser <- optparse::OptionParser(option_list = option_list)
+opt <- optparse::parse_args(opt_parser)
+
+repo <- opt$repo
+git_pat <- opt$git_pat
