54724e7a1ed9379f440a3da9a51f580a20879ff3
Candace Savonen <cansav09@gmail.com>
2022-04-01 11:59:40 -0400

    Update url-check.R

--- scripts/url-check.R
+++ scripts/url-check.R
@@ -41,7 +41,7 @@ get_urls <- function(file) {
   url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
   urls <- stringr::str_extract(content, url_pattern)
   if (length(urls) > 0 ){
-    urls <- gsub(")$|)\\.|\\),|\\)", "", urls)
+    urls <- gsub(")$|)\\.|\\),|\\)|,", "", urls)
     urls_status <- sapply(urls, test_url)
     url_df <- data.frame(urls, urls_status, file)
     return(url_df)
