590933f492104f532a63bcfc024e55883934fc01
Candace Savonen <cansav09@gmail.com>
2023-09-22 10:53:33 -0400

    Add find issue bits

---
+++ scripts/find_issue.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+
+# Written by Candace Savonen Sept 2021
+
+if (!("optparse" %in% installed.packages())){
+  install.packages("optparse")
+}
+
+library(optparse)
+
+option_list <- list(
+  optparse::make_option(
+    c("--repo"),
+    type = "character",
+    default = NULL,
+    help = "GitHub repository name, e.g. jhudsl/OTTR_Template",
+  ),
+  optparse::make_option(
+    c("--git_pat"),
+    type = "character",
+    default = NULL,
+    help = "GitHub personal access token",
+  )
+)
+
+# Read the arguments passed
+opt_parser <- optparse::OptionParser(option_list = option_list)
+opt <- optparse::parse_args(opt_parser)
+
+repo <- opt$repo
+git_pat <- opt$git_pat
+
+if (!is.character(repo)) {
+  repo <- as.character(repo)
+}
+
+install.packages('githubr')
+
+issue_titles <- get_issues(opt$repo)$title, git_pat = opt$git_pat)
+
+issue_exists <- any(grep('Broken URLs found in the course!', issue_titles))
+
+# Print out the result
+write(issue_exists, stdout())
